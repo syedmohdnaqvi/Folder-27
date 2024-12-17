27cc6cccdc020cdc63dbb0dc0e34f7e2bd13041a
syedmohdnaqvi <87820533+syedmohdnaqvi@users.noreply.github.com>
2024-12-17 13:18:11 +0530

    Add files via upload

---
+++ Shine Building Cleaning/nav.js
@@ -0,0 +1,41 @@
+document.write(`
+
+
+
+
+<header>
+<nav class="navbar navbar-expand-lg navbar-light ">
+    <div class="container-fluid">
+      <a class="navbar-brand"  href="home.html">
+        <img src="images/shinelogo.png" alt="shinelogo">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse "  id="navbarSupportedContent">
+        <ul class="navbar-nav  mb-2 mb-lg-0 shine-nav"  >
+          <li class="nav-item">
+            <a class="nav-link " aria-current="page" href="home.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="services.html">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact Us</a>
+          </li>
+          
+        </ul>
+       
+      </div>
+    </div>
+  </nav>
+</header>
+
+
+
+
+
+`)
